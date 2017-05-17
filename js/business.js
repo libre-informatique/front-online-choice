6c0dc9571e604273fe9620512dfcb7fa13ec42c3
PapsOu <vivien.frasca@libre-informatique.fr>
2017-05-17 18:13:03 +0200

    Done data flip for events structure

--- js/business.js
+++ js/business.js
@@ -2,68 +2,61 @@ $.extend(app, {
     business: {
         events: {
             manageApiResult: function (result, minInterval, maxInterval) {
-                var out = {
-                    tabs: [],
-                    events: [],
-                    tabsIds: {}
+
+                var finalFormat = {
+                    days: {},
+                    ts: {}
                 };
 
-                for (var i = 0; i <= 2; i++) {
-                    var currentDay = new Date(minInterval.getTime());
-                    currentDay.setDate(currentDay.getDate() + i);
-
-                    currentDay = moment(currentDay);
-                    currentDayId = currentDay.format("ddddDDMM");
-
-                    var tab = {
-                        date: currentDay,
-                        currentDayId: currentDayId,
-                        label: currentDay.format("dddd DD/MM"),
-                        id: i,
-                        eventsNumber: 0,
-                        ts: []
-                    };
-
-                    out.tabs.push(tab);
-
-                    out.tabsIds[currentDayId] = tab;
-                }
-
                 $.each(result, function (i, event) {
-
                     $.each(event.manifestations, function (j, manif) {
+                        $.each(manif.timeSlots, function (k, timeslot) {
+                            var tsId = timeslot.id;
+                            var ts = null;
 
-                        var min = new Date(manif.startsAt);
-                        var max = new Date(manif.endsAt);
-                        var currentDayId = moment(min).format("ddddDDMM");
-
-                        $.each(manif.timeSlots, function (k, ts) {
-
-                            var tsId = ts.id;
-
-                            // Check if we are in current date interval (today -> +2 days)
-
-                            console.info(min, minInterval, max, maxInterval);
-
-                            if (min > minInterval && max < maxInterval) {
-                                console.info(out);
-                                event.toBeManaged = true;
-
-                                if (typeof event.minDate === 'undefined' || min < event.minDate)
-                                    event.minDate = min;
-                                if (typeof event.maxDate === 'undefined' || max > event.maxDate)
-                                    event.maxDate = max;
-
-                                out.tabsIds[currentDayId].ts.push(ts);
+                            if (!finalFormat.ts.hasOwnProperty(tsId)) {
+                                finalFormat.ts[tsId] = $.extend(timeslot, {manifestations: {}});
                             }
+                            ts = finalFormat.ts[tsId];
+
+                            var mId = manif.id;
+                            var m = null;
+
+                            if (!ts.manifestations.hasOwnProperty(mId)) {
+                                ts.manifestations[mId] = $.extend(manif, {event: null});
+                                delete manif.timeSlots;
+                            }
+                            m = ts.manifestations[mId];
+
+                            m.event = event;
+
+                            delete event.manifestations;
                         });
                     });
-
-                    if (event.toBeManaged)
-                        out.events.push(event);
                 });
-                
-                return out;
+
+                Object.keys(finalFormat.ts).forEach(function (key) {
+
+                    var ts = finalFormat.ts[key];
+                    var day = moment(new Date(ts.startsAt));
+                    var dayId = day.format('ddddDDMM');
+
+                    if (!finalFormat.days.hasOwnProperty(dayId)) {
+                        finalFormat.days[dayId] = {
+                            id: day.format('ddddDDMM'),
+                            label: day.format('dddd DD/MM'),
+                            ts: []
+                        };
+                    }
+
+                    finalFormat.days[dayId].ts.push(ts);
+                });
+
+                delete finalFormat.ts;
+
+                console.info(finalFormat);
+
+                return finalFormat;
             }
         }
     }
